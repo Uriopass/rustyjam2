8e0c59bfd0b71da15f597bdd15760e5623cc3c71
Paris DOUADY <paris.douady@hotmail.fr>
2022-06-19 11:49:58 +0200

    added dog chicks

--- src/entities.rs
+++ src/entities.rs
@@ -1,7 +1,7 @@
 use crate::gfx::MouseProj;
 use crate::{Children, Entity, Parent, Query, Time, Vec2, Vec3, Without};
-use bevy::math::{vec2, vec3, Vec3Swizzles};
-use bevy::prelude::{AssetServer, Commands, Component, Res, SpriteBundle, Transform};
+use bevy::math::{vec2, vec3, Rect, Vec3Swizzles};
+use bevy::prelude::*;
 
 #[derive(Component)]
 pub struct Wolf;
@@ -12,19 +12,33 @@ pub struct Chicken;
 #[derive(Component)]
 pub struct Dog;
 
-pub enum LookerState {
+#[derive(Component)]
+pub struct DogChick;
+
+#[derive(Copy, Clone)]
+pub enum LookerLocation {
     Inside,
     Outside,
+}
+
+#[derive(Copy, Clone)]
+pub enum LookerState {
+    Happy,
+    HappyInside,
+    Scared { until: f64 },
+    ScaredInside { until: f64 },
 }
 
 #[derive(Component)]
 pub struct Looker {
     spawn_point: Vec2,
     spawn_door: Vec2,
-    scared_until: Option<f64>,
     state: LookerState,
+    location: LookerLocation,
     getaway: Vec2,
     randobjective: Option<Vec2>,
+    confined_within: Rect<f32>,
+    merged: bool,
 }
 
 #[derive(Component)]
@@ -39,8 +53,33 @@ pub struct Speed(pub f32);
 #[derive(Component)]
 pub struct BobAnim(pub f32);
 
-pub fn collision_avoidance(mut toavoid: Query<(Entity, &mut Looker, &Transform)>) {
-    for (_, mut looker, _) in toavoid.iter_mut() {
+const OUTSIDE: Rect<f32> = Rect {
+    left: -1000.0,
+    right: 1000.0,
+    top: 1000.0,
+    bottom: -650.0,
+};
+
+const DOGCHICK_ENCLOT: Rect<f32> = Rect {
+    left: -217.0,
+    right: 217.0,
+    top: -720.0,
+    bottom: -1000.0,
+};
+
+pub fn collision_avoidance(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+    mut toavoid: Query<(
+        Entity,
+        &mut Looker,
+        &Transform,
+        Option<&Chicken>,
+        Option<&Dog>,
+        Option<&DogChick>,
+    )>,
+) {
+    for (_, mut looker, _, _, _, _) in toavoid.iter_mut() {
         looker.getaway = Vec2::ZERO;
     }
 
@@ -52,6 +91,50 @@ pub fn collision_avoidance(mut toavoid: Query<(Entity, &mut Looker, &Transform)>
             let force = 10.0 * diff / dist2;
             a.1.getaway -= force;
             b.1.getaway += force;
+            if a.3.is_some() && b.4.is_some() && !a.1.merged && !b.1.merged {
+                a.1.merged = true;
+                b.1.merged = true;
+                commands.entity(a.0).despawn_recursive();
+                commands.entity(b.0).despawn_recursive();
+
+                let dogchickpos = a.2.translation + vec3(diff.x, diff.y, 0.0) / 2.0;
+
+                let dogchick = commands
+                    .spawn()
+                    .insert(AiResult {
+                        target_speed: 10.0,
+                        target_dir: vec2(0.0, 0.0),
+                    })
+                    .insert(Looker {
+                        spawn_point: b.1.spawn_point + vec2(500.0, 0.0),
+                        spawn_door: b.1.spawn_door + vec2(500.0, 0.0),
+                        state: LookerState::HappyInside,
+                        location: LookerLocation::Outside,
+                        getaway: vec2(0.0, 0.0),
+                        randobjective: None,
+                        confined_within: DOGCHICK_ENCLOT,
+                        merged: false,
+                    })
+                    .insert(Speed(0.0))
+                    .insert(DogChick)
+                    .insert_bundle(SpriteBundle {
+                        transform: Transform::default()
+                            .with_translation(dogchickpos)
+                            .with_scale(vec3(0.6, 0.6, 1.0)),
+                        texture: asset_server.load("shadow.png"),
+                        ..Default::default()
+                    })
+                    .id();
+                commands
+                    .spawn()
+                    .insert(Parent(dogchick))
+                    .insert(BobAnim(fastrand::f32() * 32.0))
+                    .insert_bundle(SpriteBundle {
+                        transform: Transform::default().with_scale(Vec3::new(1.0, 1.0, 1.0)),
+                        texture: asset_server.load("dogchick.png"),
+                        ..Default::default()
+                    });
+            }
         }
     }
 }
@@ -69,46 +152,51 @@ pub fn dogchick_ai(
             let newpos =
                 pos + vec2(fastrand::f32() - 0.5, fastrand::f32() - 0.5).normalize() * 100.0;
             looker.randobjective = Some(newpos);
-            if newpos.y < -650.0 || newpos.x.abs() > 1000.0 {
+            let r = looker.confined_within;
+            if newpos.y < r.bottom || newpos.y > r.top || newpos.x < r.left || newpos.x > r.right {
                 looker.randobjective = None;
             }
         }
 
-        if let Some(until) = looker.scared_until {
-            if until > time.seconds_since_startup() {
-                looker.scared_until = None;
+        use LookerLocation::*;
+        use LookerState::*;
+        match looker.state {
+            Happy => {}
+            HappyInside => {}
+            Scared { until } => {
+                if until > time.seconds_since_startup() {
+                    looker.state = Happy;
+                }
+            }
+            ScaredInside { until } => {
+                if until > time.seconds_since_startup() {
+                    looker.state = HappyInside;
+                }
             }
         }
-
-        let objective = match looker.state {
-            LookerState::Inside => {
-                if looker.scared_until.is_some() {
-                    looker.spawn_point
-                } else {
-                    looker.spawn_door
+        let objective = match (looker.location, looker.state) {
+            (Inside, HappyInside) => looker.randobjective.unwrap_or(pos),
+            (Inside, Happy) | (Outside, HappyInside) => looker.spawn_door,
+            (Inside, Scared { .. } | ScaredInside { .. }) => looker.spawn_point,
+            (Outside, Scared { .. } | ScaredInside { .. }) => looker.spawn_door,
+            (Outside, Happy) if inp.0.distance(pos) < 110.0 => {
+                max_speed = 100.0;
+                let mut obj = inp.0;
+                if obj.y < -630.0 {
+                    obj.y = -630.0;
                 }
+                obj
             }
-            LookerState::Outside => {
-                if looker.scared_until.is_some() {
-                    looker.spawn_door
-                } else if inp.0.distance(pos) > 100.0 {
-                    looker.randobjective.unwrap_or(pos)
-                } else {
-                    max_speed = 100.0;
-                    let mut obj = inp.0;
-                    if obj.y < -630.0 {
-                        obj.y = -630.0;
-                    }
-                    obj
-                }
-            }
+            (Outside, Happy) => looker.randobjective.unwrap_or(pos),
         };
 
         if looker.spawn_door.distance(pos) < 5.0 {
-            if looker.scared_until.is_some() {
-                looker.state = LookerState::Inside;
+            looker.location = match looker.state {
+                Happy => Outside,
+                HappyInside => Inside,
+                Scared { .. } => Inside,
+                ScaredInside { .. } => Inside,
             }
-            looker.state = LookerState::Outside;
         }
 
         let to_obj = objective - pos;
@@ -127,6 +215,8 @@ pub fn dogchick_ai(
         speed.0 += (res.target_speed - speed.0).min(50.0 * time.delta_seconds());
     }
 }
+
+pub fn mutant_ai() {}
 
 pub fn speedbob(
     time: Res<Time>,
@@ -193,17 +283,19 @@ pub fn start_game(mut commands: Commands, asset_server: Res<AssetServer>) {
             .insert(Looker {
                 spawn_point: vec2(x, y),
                 spawn_door: vec2(-500.0 + 100.0 * (fastrand::f32() - 0.5), -650.0),
-                scared_until: None,
-                state: LookerState::Inside,
+                state: LookerState::Happy,
+                location: LookerLocation::Inside,
                 getaway: Default::default(),
                 randobjective: None,
+                confined_within: OUTSIDE,
+                merged: false,
             })
             .insert(AiResult {
                 target_speed: 10.0,
                 target_dir: vec2(0.0, 0.0),
             })
             .insert(Speed(0.0))
-            .insert(Wolf)
+            .insert(Dog)
             .id();
 
         commands
@@ -233,9 +325,11 @@ pub fn start_game(mut commands: Commands, asset_server: Res<AssetServer>) {
             .insert(Looker {
                 spawn_point: vec2(x, y),
                 spawn_door: vec2(500.0 + 100.0 * (fastrand::f32() - 0.5), -650.0),
-                scared_until: None,
-                state: LookerState::Inside,
+                state: LookerState::Happy,
+                location: LookerLocation::Inside,
                 getaway: Default::default(),
+                confined_within: OUTSIDE,
+                merged: false,
                 randobjective: None,
             })
             .insert(AiResult {
